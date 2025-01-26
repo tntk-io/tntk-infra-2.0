318903a7272cb997ac1c444004a0b406a0e5faa4
ernram <135255952+ernram@users.noreply.github.com>
2025-01-26 09:33:51 -0600

    add new vars

--- generate-overrides.sh
+++ generate-overrides.sh
@@ -6,6 +6,7 @@ read -p "Enter AWS Region (e.g., us-east-2): " aws_region
 read -p "Enter AWS Account ID: " aws_account_id
 read -p "Enter Base Domain (e.g., dev.ernestdevops.net): " base_domain
 read -p "Enter Tag Environment (e.g., dev): " tag_env
+read -p "Datadog Enabled (true/false): " datadog_enabled
 read -p "Enter Datadog API Key: " datadog_api_key
 read -p "Enter Datadog Application Key: " datadog_application_key
 read -p "Enter Datadog Region (e.g., us5.datadoghq.com): " datadog_region
@@ -21,6 +22,7 @@ aws_region = "$aws_region"
 aws_account_id = "$aws_account_id"
 base_domain = "$base_domain"
 tag_env = "$tag_env"
+datadog_enabled = "$datadog_enabled"
 datadog_api_key = "$datadog_api_key"
 datadog_application_key = "$datadog_application_key"
 datadog_region = "$datadog_region"
@@ -29,14 +31,14 @@ github_name = "$github_name"
 github_organization = "$github_organization"
 github_token = "$github_token"
 
-# JSON variables
+# GitHub Repositories that will be imported into ArgoCD
 argocd_repos = {
   tntk-cd = {
     repo_url = "https://github.com/${github_organization}/tntk-cd"
     name     = "tntk-cd"
   }
 }
-
+# ArgoCD Applications that will be created by Terraform in ArgoCD
 argocd_apps = {
   shared-resources = {
     name      = "shared-resources"
@@ -50,7 +52,7 @@ argocd_apps = {
     }
     source = {
       repo_url        = "https://github.com/${github_organization}/tntk-cd"
-      chart           = "charts/dev-resources"
+      chart           = "charts/shared-resources"
       target_revision = "0.0.1"
     }
     helm = {
@@ -58,88 +60,28 @@ argocd_apps = {
       value_files_path = ["values.yaml"]
     }
   }
-  tntk-web-dev = {
-    name      = "tntk-api-dev"
+  tntk-bookapp = {
+    name      = "tntk-bookapp"
     namespace = "argocd"
     labels = {
-      environment = "dev"
+      environment = "$tag_env"
     }
     destination = {
       server    = "https://kubernetes.default.svc"
-      namespace = "dev"
+      namespace = "$tag_env"
     }
     source = {
-      repo_url        = "https://github.com/${github_organization}/tntk-web"
-      chart           = "charts/tntk-web"
-      target_revision = "0.0.1"
+      repo_url        = "https://github.com/${github_organization}/tntk-cd"
+      chart           = "charts/tntk-bookapp"
+      target_revision = "0.1.0"
     }
     helm = {
-      release_name     = "tntk-api"
-      value_files_path = ["../../environments/values-dev.yaml"]
-    }
-  }
-  tntk-orders-dev = {
-    name      = "tntk-web-dev"
-    namespace = "argocd"
-    labels = {
-      environment = "dev"
-    }
-    destination = {
-      server    = "https://kubernetes.default.svc"
-      namespace = "dev"
-    }
-    source = {
-      repo_url        = "https://github.com/${github_organization}/tntk-orders"
-      chart           = "charts/tntk-orders"
-      target_revision = "0.0.1"
-    }
-    helm = {
-      release_name     = "tntk-web"
-      value_files_path = ["../../environments/values-dev.yaml"]
-    }
-  }
-  tntk-auth-dev = {
-    name      = "tntk-auth-dev"
-    namespace = "argocd"
-    labels = {
-      environment = "dev"
-    }
-    destination = {
-      server    = "https://kubernetes.default.svc"
-      namespace = "dev"
-    }
-    source = {
-      repo_url        = "https://github.com/${github_organization}/tntk-auth"
-      chart           = "charts/tntk-auth"
-      target_revision = "0.0.1"
-    }
-    helm = {
-      release_name     = "tntk-api"
-      value_files_path = ["../../environments/values-dev.yaml"]
-    }
-  }
-  tntk-products-dev = {
-    name      = "tntk-products-dev"
-    namespace = "argocd"
-    labels = {
-      environment = "dev"
-    }
-    destination = {
-      server    = "https://kubernetes.default.svc"
-      namespace = "dev"
-    }
-    source = {
-      repo_url        = "https://github.com/${github_organization}/tntk-products"
-      chart           = "charts/tntk-products"
-      target_revision = "0.0.1"
-    }
-    helm = {
-      release_name     = "tntk-products"
-      value_files_path = ["../../environments/values-dev.yaml"]
+      release_name     = "tntk-bookapp"
+      value_files_path = ["../../environments/dev/values.yaml"]
     }
   }
 }
-
+# AWS IAM Authentication Config for the EKS cluster (this is how you add users to the cluster)
 aws_auth_config = {
   roles = []
   users = [
@@ -152,6 +94,112 @@ aws_auth_config = {
   accounts = [
     "${aws_account_id}"
   ]
+}
+
+# ECR repos that will be created by Terraform
+ecr_repos = {
+  tntk-web = {
+    name         = "tntk-web"
+    count_number = 10
+  }
+  tntk-orders = {
+    name         = "tntk-orders"
+    count_number = 10
+  }
+  tntk-auth = {
+    name         = "tntk-auth"
+    count_number = 10
+  }
+  tntk-products = {
+    name         = "tntk-products"
+    count_number = 10
+  }
+}
+
+# EKS cluster that will be created by Terraform
+eks_settings = {
+  cluster = {
+    name                                     = "final-project"
+    version                                  = "1.31"
+    cluster_endpoint_public_access           = true
+    enable_cluster_creator_admin_permissions = true
+  }
+  cluster_addons = {
+    eks-pod-identity-agent = {
+      most_recent = true
+    }
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+  }
+  node_group_defaults = {
+    instance_types = ["t3a.small"]
+  }
+  # EKS node groups that will be created by Terraform
+  managed_node_groups = {
+    tntk_eks_nodes = {
+      min_size       = 2
+      max_size       = 2
+      desired_size   = 2
+      instance_types = ["t3a.small"]
+      capacity_type  = "SPOT"
+    }
+  }
+}
+
+# Repositories to create in your GitHub organization for the project
+repositories = {
+  tntk-auth = {
+    description  = "Auth application for the final project"
+    visibility   = "private"
+    has_issues   = false
+    has_projects = false
+    has_wiki     = false
+    clone_url    = "https://github.com/tntk-io/tntk-auth"
+  }
+  tntk-cd = {
+    description  = "Manifests for the final project"
+    visibility   = "public"
+    has_issues   = false
+    has_projects = false
+    has_wiki     = false
+    clone_url    = "https://github.com/tntk-io/tntk-cd-2.0"
+  }
+  tntk-orders = {
+    description  = "Orders application for the final project"
+    visibility   = "private"
+    has_issues   = false
+    has_projects = false
+    has_wiki     = false
+    clone_url    = "https://github.com/tntk-io/tntk-orders"
+  }
+  tntk-products = {
+    description  = "Products application for the final project"
+    visibility   = "private"
+    has_issues   = false
+    has_projects = false
+    has_wiki     = false
+    clone_url    = "https://github.com/tntk-io/tntk-products"
+  }
+  tntk-web = {
+    description  = "Web application for the final project"
+    visibility   = "private"
+    has_issues   = false
+    has_projects = false
+    has_wiki     = false
+    clone_url    = "https://github.com/tntk-io/tntk-web-2.0"
+  }
+}
+
+# Tags to be applied to Terraform resources
+tags = {
+  Environment = "$tag_env"
 }
 
 # Additional JSON variables can be added here
