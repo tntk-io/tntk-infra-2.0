dcc410653957b6036248463b77d3c1fa25af2986
ernram <135255952+ernram@users.noreply.github.com>
2025-01-26 09:32:46 -0600

    make these toggle-able

--- 10-github-actions-runner-controller.tf
+++ 10-github-actions-runner-controller.tf
@@ -4,6 +4,7 @@
 
 # This resource is used to install the controller for the actions runner scale set
 resource "helm_release" "gha_actions_runner_controller" {
+  count            = var.github_self_hosted_runners_enabled ? 1 : 0
   name             = "gha-runner-scale-set-controller"
   chart            = "oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set-controller"
   namespace        = "actions-runner-system"
@@ -14,7 +15,7 @@ resource "helm_release" "gha_actions_runner_controller" {
 
 # This resource is used to install the actions runner scale set for each repository
 resource "helm_release" "gha_actions_runner_scale_set" {
-  for_each         = var.repositories
+  for_each         = var.github_self_hosted_runners_enabled ? var.repositories : {}
   name             = "gha-runner-scale-set-${each.key}"
   chart            = "oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set"
   namespace        = "github-actions"
